15ce41d4fa97280acdb21563aaa9fbe4db751ba2
Jorge Rodríguez <contactasJorGii@gmail.com>
2024-03-04 11:30:06 +0100

    Add files via upload

---
+++ Ventas/test/VentasTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import ventas.*;
+
+public class VentasTest {
+
+	public static void main(String[] args) {
+		Producto producto1 = new Producto("camisa", 50.00);
+		Producto producto2 = new Producto("pantalon", 100.00);
+		
+		Orden orden1 = new Orden();
+		orden1.agregarProducto(producto1);
+		orden1.agregarProducto(producto2);
+		orden1.mostrarOrden();
+
+	}
+
+}
